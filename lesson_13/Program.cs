2a15b89ba6d02cf3025b29d1a8ee553cbf000aaf
Miroslava <miroslavaroshchina@gmail.com>
2020-10-02 16:12:52 +0300

    Добавьте файлы проекта.

---
+++ lesson_13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lesson_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x >= 0)
+            {
+                Console.WriteLine(2 * x - 10);
+            }
+            else
+            {
+                Console.WriteLine((2*Math.Abs(x))-1);
+            }
+        
+        }
+    
+    }
+
+}
---
+++ lesson_14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lesson_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            n = n % 7;
+                switch (n)
+                {
+                    case 1%7:
+                        Console.WriteLine("Понедельник"); //если знач n % 7 =  1%7, тогда вывед "пон" (остаток от деления будет одинаковый)
+                    break;
+                    case 2%7:
+                        Console.WriteLine("Вторник");
+                        break;
+                    case 3%7:
+                        Console.WriteLine("Среда");
+                        break;
+                    case 4%7:
+                        Console.WriteLine("Четверг");
+                        break;
+                    case 5%7:
+                        Console.WriteLine("Пятница");
+                        break;
+                    case 6%7:
+                        Console.WriteLine("Суббота");
+                        break;
+                    case 7%7:
+                        Console.WriteLine("Воскресенье");
+                        break;
+                }
+            
+        }
+    }
+}
+    
+
