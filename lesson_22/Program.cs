60cae8f050b17b2d0dc80dc4667ab3901f9d1968
Miroslava <miroslavaroshchina@gmail.com>
2020-10-04 21:51:30 +0300

    do while 3 (кратные числа из списка в квадрате)

---
+++ lesson_22/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lesson_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            do
+                if (A % 2 == 0)
+                {
+                    int C = A*A;
+                    Console.Write("{0} ", C);
+                        A++;
+
+                }
+                else
+                {
+                    A++;
+                }
+            while (A <= B);
+        }
+    }
+}
