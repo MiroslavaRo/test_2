d8b48f7aa03638eb69dbe5881ffaa3036f0e9728
Miroslava <miroslavaroshchina@gmail.com>
2020-10-05 00:14:04 +0300

    поправка

--- lesson_23/Program.cs
+++ lesson_23/Program.cs
@@ -10,15 +10,28 @@ namespace lesson_23
             double B = double.Parse(Console.ReadLine());
             double C = A*B;
             int d = 0;
-            for (int i = 3; i <= C; i += 3)
-            {
-                d++;
 
-//                Console.WriteLine(i);
+            for (int i = 1; i <= C; i++)
+            {
+                C -= 3;
+                d = i;
             }
-            Console.WriteLine("дня {0}", d);
+            if (d == 1)
+            {
+                Console.WriteLine("дня {0}", d);
+            }
+            else
+            {
+                Console.WriteLine("дня {0}", d + 1);
+            }
+                
         }
-    }
+
+            
+        }
+            
+        
+    
 }
 /*В компьютер князя Владимира поступили сведения о количестве змеев-горынычей и количестве голов у каждого из них. 
 Илья Муромец срубает 3 головы в день. 
