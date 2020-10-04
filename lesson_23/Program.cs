2c75e4e83b4f6b50b5685696911e8284f2d52f56
Miroslava <miroslavaroshchina@gmail.com>
2020-10-04 22:46:53 +0300

    for 2

---
+++ lesson_23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lesson_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+            double C = A*B;
+            int d = 0;
+            for (int i = 3; i <= C; i += 3)
+            {
+                d++;
+
+//                Console.WriteLine(i);
+            }
+            Console.WriteLine("дня {0}", d);
+        }
+    }
+}
+/*В компьютер князя Владимира поступили сведения о количестве змеев-горынычей и количестве голов у каждого из них. 
+Илья Муромец срубает 3 головы в день. 
+Используя цикл For решите, за сколько дней богатырь срубит все головы*/
