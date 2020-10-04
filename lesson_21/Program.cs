fde9639a94e3cf6201b0d133191b56ec8f3794e5
Miroslava <miroslavaroshchina@gmail.com>
2020-10-04 21:38:16 +0300

    do while (нечетные, кратные 5 числа из диапозона)

---
+++ lesson_21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lesson_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());            
+            do
+                if (A % 2!= 0 && A % 5 == 0)
+                {
+                    Console.Write("{0} ", A++);
+
+                }
+                else
+                {
+                    A++;
+                }
+            while (A <= B);
+        }
+    }
+}
