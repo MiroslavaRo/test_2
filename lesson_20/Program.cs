22541b9e29342d3180fa30233aa80d76427e9654
Miroslava <miroslavaroshchina@gmail.com>
2020-10-04 21:27:45 +0300

    do while

---
+++ lesson_20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lesson_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+           // int C = A + 1;
+            do
+                if (A % 2 == 0 && A % 3 == 0)
+                {
+                    Console.WriteLine("{0} ", A++);
+                    
+                }
+            else
+                {
+                    A++;
+                }
+
+            
+            while (A < B);
+        }
+    }
+}
