38915be666ed7e89c56f3b7ed79e3545d8a54ebc
Miroslava <miroslavaroshchina@gmail.com>
2020-10-05 17:38:18 +0300

    array with random elements (Next() method)

---
+++ Lesson_24/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(); //инициализируем генератор случайных чисел
+            int[] myArray;
+            int n = rnd.Next(5, 10); //генерируем случайное число из диапазона [5..10)
+            myArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                myArray[i] = rnd.Next(10); // заполняем массив случайными числами
+            }
+            foreach (int elem in myArray) //выводим массив на экран
+            {
+                Console.Write("{0} ", elem);
+            }
+        }
+    }
+}
