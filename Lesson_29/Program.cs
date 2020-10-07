8c687ae70837df3954322ee114f57b3a682780e6
Miroslava <miroslavaroshchina@gmail.com>
2020-10-07 15:26:02 +0300

    Sum of elements in array (not void methods)

---
+++ Lesson_29/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson_29
+{
+    class Program
+    {
+        static int[] Input()
+        {
+            Console.Write("n= ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < a.Length; ++i)
+            {
+                Console.Write("a[{0}]= ", i);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            return a;
+        }
+        static int Sum(int[] a)
+        {
+            int sum = 0;
+            foreach (int elem in a)
+            {
+                sum += elem;
+            }
+            return sum;
+        }
+        static void Main()
+        {
+            int[] a = Input();
+            Console.WriteLine("Сумма элементов массива={0}", Sum(a));
+        }
+    }
+}
