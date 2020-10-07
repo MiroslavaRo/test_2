18b045d92af6e33eab730596511795e6f0eceeb0
Miroslava <miroslavaroshchina@gmail.com>
2020-10-07 15:08:04 +0300

    максимальное число в массиве/в каждой строке массива

---
+++ Lesson_28/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Lesson_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("m= ");
+            int m = int.Parse(Console.ReadLine());
+            int[,] a = new int[n, m];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(0); j++)
+                {
+                    Console.Write("a[{0},{1}]= ", i, j);
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            int max = 0;
+            for (int n2 = 0; n2 < a.GetLength(0); n2++)
+            {
+                for (int f = 0; f < a.GetLength(1); f++)
+                {
+                    max = max < a[n2, f] ? a[n2, f] : max;
+                    
+                }
+                Console.Write(max + " "); //выведет максимальные значения в каждой СТРОКЕ
+            }
+
+           // Console.Write(max + " ");//выведет макс значение ВО ВСЕМ МАССИВЕ
+        }
+    }
+}
