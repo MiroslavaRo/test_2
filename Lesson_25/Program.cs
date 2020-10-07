aa10dbe97309a3ae308a1083cabfb00770ee4fa2
Miroslava <miroslavaroshchina@gmail.com>
2020-10-07 13:27:58 +0300

    sum of elem in array

---
+++ Lesson_25/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type quantity of elements in array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            float[] A = new float[n];
+            float sum;
+            int i = 0;
+            Console.WriteLine("Type elements of array: ");
+            while (i < n)
+            {
+                A[i] = Convert.ToInt32(Console.ReadLine());
+                i++;
+            }
+
+            sum = 0;
+            i = 0;
+            while (i < n)
+            {
+                sum += A[i];
+                i++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
