c2e227061699c0acafa662b683d71844aaa3283d
Miroslava <miroslavaroshchina@gmail.com>
2020-10-16 02:07:21 +0300

    переворачивание строки

---
+++ Lesson_31/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson_31
+{
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                string fwd = Console.ReadLine();
+                string rvs = Reverse(fwd);
+                Console.WriteLine(rvs);
+                Console.ReadKey();
+
+            }
+
+            private static string Reverse(string fwd)
+            {
+            char[] carr = fwd.ToCharArray();            
+            char temp;            
+            int j= carr.Length - 1;
+            for (int i = 0; i < j; i++)
+                {
+                temp = carr[i];
+                carr[i] =carr[j];
+                carr[j] = temp;
+                j--;
+            }
+            return new string(carr);
+            }
+        }
+    
+}
