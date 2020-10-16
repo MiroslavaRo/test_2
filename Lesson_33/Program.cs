305c1ecaa9f417184c25944885534df43b62cb82
Miroslava <miroslavaroshchina@gmail.com>
2020-10-17 00:23:48 +0300

    верзний и нижний регистр символа

---
+++ Lesson_33/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] ch = str.ToCharArray();
+            for (int i =0; i < ch.Length; i++)
+            {
+                if (char.IsLetter(ch[i]))
+                {
+                    if (char.IsUpper(ch[i]))
+                    {
+                        ch[i] = char.ToLower(ch[i]);
+                    }
+                    else
+                    {
+                        ch[i] = char.ToUpper(ch[i]);
+                    }
+                }
+            }
+            Console.WriteLine(ch);
+        }
+    }
+}
