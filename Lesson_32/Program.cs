fb4bf53e8981b0f3b17732fbff1172be8bbaabf9
Miroslava <miroslavaroshchina@gmail.com>
2020-10-16 23:52:17 +0300

    считает гласные и согласные

---
+++ Lesson_32/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lesson_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] ch = str.ToCharArray();
+            Console.WriteLine($"Гласных {Vovels(ch)}\nСогласных {(Consonants(ch)- Vovels(ch))}");
+        }
+        static int Vovels(char[] ch)
+        {
+            char[] vovels = { 'у', 'е', 'а', 'о', 'ы', 'я', 'и', 'ю', 'э' };
+            int gl = 0;
+            for (int i = 0; i < ch.Length; i++)
+            {
+                for (int j = 0; j < vovels.Length; j++)
+                {
+                    if (ch[i] == vovels[j])
+                    {
+                        gl++;
+                       
+                    }
+
+                }
+            }
+            return gl;
+        }
+        static int Consonants(char[] ch)
+        {
+            char[] vovels = { 'у', 'е', 'а', 'о', 'ы', 'я', 'и', 'ю', 'э' };
+            int sog=0;
+            for (int i =0; i < ch.Length; i++)
+            {
+                if (char.IsLetter(ch[i]))
+                {
+                    sog++;
+                   
+                }
+            }
+
+            return sog;
+        }
+    }
+}
