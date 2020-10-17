8d91996809a48fa5af031d97d75706939a198049
Miroslava <miroslavaroshchina@gmail.com>
2020-10-18 00:54:58 +0300

    index

---
+++ Lesson_35/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int index = 0;
+            string str = Console.ReadLine();
+            char[] div = {' '};
+            string []s = str.Split(div);
+            for(int i =0; i < s.Length; i++)
+            {                
+                max = max > s[i].Length ? max : s[i].Length;
+                index = i;               
+            }
+            Console.WriteLine(s[index]);
+
+        }
+    }
+}
