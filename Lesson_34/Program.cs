a5e8773fbbb34c31308a3ca49e4ad236eff62eeb
Miroslava <miroslavaroshchina@gmail.com>
2020-10-18 00:31:07 +0300

    поллиндром

---
+++ Lesson_34/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lesson_34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] div = { ' ' };
+            
+            string[] parts = str.Split(div);
+            for (int i = 0; i < parts.Length; i++)
+            {
+                char[] ch = parts[i].ToCharArray();
+                if (P(ch))
+                {
+                    Console.WriteLine(parts[i]);
+                }                     
+            }           
+
+        }
+        static bool P(char[] ch)
+        {
+            bool p = false;
+            for (int j = 0; j < ch.Length; j++)
+            {
+                if (ch[j] == ch[ch.Length - 1 - j])
+                {
+                    p = true;
+                }
+                else
+                {
+                    p = false; //если хотя бы один false в цикле - все выражение false
+
+                }
+            }
+            return p;
+        }
+      
+    }
+}
